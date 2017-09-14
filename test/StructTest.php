a757c8fa6ffa474612628374e5719bb79a6b3da0
Rik Meijer <rik@pulledbits.org>
2017-09-14 13:51:02 +0200

    add test for map method

--- test/StructTest.php
+++ test/StructTest.php
@@ -14,4 +14,11 @@ class StructTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('bar', $struct->foo);
     }
 
+    public function testMap_When_IdentifierGiven_Expect_PropertySet() {
+        $struct = new Struct('foo');
+
+        $struct->map('foo', 'bar');
+
+        $this->assertEquals('bar', $struct->foo);
+    }
 }
